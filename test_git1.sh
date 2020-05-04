f6b90f972a016716f70d20cae2ad48ac531d8c18
sudiplamsal00 <sudiplamsal.00@gmail.com>
2020-05-04 14:25:19 +0545

    see the changes

--- test_git1.sh
+++ test_git1.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
+main_function()
+{
+	read -p "what do you want command do you want to test?" insert
+	sleep 2
+	echo "you have : $insert" 
+	sleep 1
+	test
+	echo $insert
+	sleep 10
+	test
+	$insert
+	echo 
+	echo
+	sleep 2
+	echo "Thank you!!"
+}
+main_function
+echo "do you want to continue? (y/n)"
+read input
+if [ $input = "y" ] #| [ $input = "Y" ]
+then
+	echo "contining..............."
+	sleep 7
+	input
+else
+		echo "exiting"	
+fi
 
-read -p "what do you want to insert in git?" insert
-sleep 2
-echo "you have insterted : $insert" 
-echo 
-sleep 2
-echo 
-echo
-echo "Thank you!!"
+#<<EOF
+test()
+{
+	PID=$!
+	i=1
+	sp="/-\|"
+	echo -n ' '
+	while [ -d /proc/$PID ]
+	do
+  		printf "\b${sp:i++%${#sp}:1}"
+	done
+}
+#EOF
